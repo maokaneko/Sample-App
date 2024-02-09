229b9c1558e3b7faa286828efe4da632c373191f
maokaneko <mkch1999@gmail.com>
2024-02-09 19:26:40 +0900

    Add user microposts

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -20,7 +20,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["user@realdomain.com"], mail.from
+    assert_equal ["mkch1999@gmail.com"], mail.from
     assert_match user.reset_token,        mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
